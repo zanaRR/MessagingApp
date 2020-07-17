b375cf753dfaf6d87a6d6cfc02e8822c1a206049
Zana <zanna._@hotmail.com>
2020-07-17 16:40:56 +0200

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Zana Ramizi on 7/17/20.
+//  Copyright © 2020 Zana Ramizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
